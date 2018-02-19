0b9ed05e3e45cf56f615ee790b8fc1cadca49d29
Bronte27 <nveldhoven@gmail.com>
2018-02-19 11:46:23 +1100

    Remove debug code

--- index.php
+++ index.php
@@ -66,7 +66,6 @@ $app->add(new TokenAuthentication([
 
 //Enable CORS
 $app->options('/{name:.+}', function($request, $response, $args){
-    echo "cors";
     return $response->withHeader('Access-Control-Allow-Origin', '*')
         ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
         ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
